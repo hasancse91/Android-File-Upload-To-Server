23f91ad6db84f269f24c5e870890815a605e6b02
Hasan Abdullah <hasan.cse91@gmail.com>
2017-08-22 06:18:00 +0600

    Update upload.php

--- file_upload_api/upload.php
+++ file_upload_api/upload.php
@@ -1,5 +1,5 @@
 <?php
-    $target_dir = "files/"; //folder name
+    $target_dir = "files/"; //folder name where your files will be stored. create this folder inside "file_upload_api" folder
     $target_file = $target_dir . basename($_FILES["file"]["name"]);
 
     $response = array('success' => false, 'message' => 'Sorry, there was an error uploading your file.');
